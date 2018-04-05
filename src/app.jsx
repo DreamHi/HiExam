5bbc76a4a363c3d71e8839b26e1d7ef4669cf430
sl_yang <shengli1222@163.com>
2018-04-06 02:08:27 +0900

    追加Home画面

---
+++ src/app.jsx
@@ -0,0 +1,47 @@
+import 'antd/dist/antd.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import SLogin from './modules/system/scenes/SLogin';
+import SHome from './modules/system/scenes/SHome';
+import SNotFound from './modules/system/scenes/SNotFound';
+
+import storage from './util/storage';
+import { ROUTE_HOME, ROUTE_LOGIN } from './util/constants';
+import './static/css/index.css';
+
+const rootElement = document.getElementById('root');
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => storage.isAuthenticatedUser()
+        ? <Component {...props} />
+        : <Redirect to={ROUTE_LOGIN} />}
+    />
+  )
+};
+
+const PublicRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => !storage.isAuthenticatedUser()
+        ? <Component {...props} />
+        : <Redirect to={ROUTE_HOME} />}
+    />
+  );
+};
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <PublicRoute path="/" exact component={SLogin} />
+      <PublicRoute path={ROUTE_LOGIN} component={SLogin} />
+      <PrivateRoute path={ROUTE_HOME} component={SHome} />
+      <Route path="*" component={SNotFound} />
+    </Switch>
+  </Router>,
+  rootElement,
+);
---
+++ src/util/constants.js
@@ -0,0 +1,7 @@
+// client router
+export const ROUTE_LOGIN = '/login';
+export const ROUTE_HOME = '/home';
+
+
+// api list
+export const API_LOGIN = '/login';
