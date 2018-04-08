1a281c522f0ef9fdc8405c7a81754f186f400a66
sl-say <sl_say2016@qq.com>
2018-04-08 18:20:19 +0900

    test heroku

---
+++ heroku/app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const indexPath = path.join(__dirname, '/dist/index.html');
+const publicPath = express.static(path.join(__dirname, '/dist'));
+const port = process.env.PORT || 8080;
+
+app.use('/', publicPath);
+app.get('/', function (_, res) { res.sendFile(indexPath) });
+
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
